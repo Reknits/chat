65378cab179c65a8df82bee9be520f45f904336e
Jimmy <42733125+Reknits@users.noreply.github.com>
2018-12-28 13:12:27 +0100

    added ooc and removed errror

--- chat/sv_chat.lua
+++ chat/sv_chat.lua
@@ -3,43 +3,42 @@ RegisterServerEvent('chat:addTemplate')
 RegisterServerEvent('chat:addMessage')
 RegisterServerEvent('chat:addSuggestion')
 RegisterServerEvent('chat:removeSuggestion')
---RegisterServerEvent('_chat:messageEntered')
 RegisterServerEvent('chat:clear')
---RegisterServerEvent('__cfx_internal:commandFallback')
 
-AddEventHandler('_chat:messageEntered', function(author, color, message)
-    if not message or not author then
-        return
-    end
-   
 
-    TriggerEvent('chatMessage', source, author, message)
 
-    if not WasEventCanceled() then
-        TriggerClientEvent('chatMessage', -1, author,  { 255, 255, 255 }, message)
-    end
-
-    print(author .. ': ' .. message)
-end)
 
 RegisterCommand('twt', function(source, args, rawCommand)
     local playerName = GetCharacterName(source)
     local msg = rawCommand:sub(4)
     TriggerClientEvent('chat:addMessage', -1, {
-        template = '<div style="padding: 1.1vw; margin: 1.0vw; background-color: rgba(62, 174, 244, 0.7);border-radius:12px;"><i class="fab fa-twitter" size: 7x></i> ^0@{0}^0:<br>{1}</br></div>',
+        template = '<div style="padding: 1.1vw; margin: 1.0vw; background-color: rgba(62, 174, 244, 0.7);border-radius:12px;">^0<i class="fab fa-twitter" size: 7x></i> @{0}^0:<br>{1}</br></div>',
         args = { playerName, msg }
     })
 end, false)
 
---[[RegisterCommand('unicorn', function(source, args, rawCommand)
+
+RegisterCommand('ooc', function(source, args, rawCommand)
+    local playerName = GetPlayerName(source)
+    local msg = rawCommand:sub(4)
+    TriggerClientEvent('chat:addMessage', -1, {
+        template = '<div style="padding: 1.1vw; margin: 1.0vw; background-color: rgba(177, 239, 185, 0.7);border-radius:12px;">^0<i class="fas fa-globe" size: 7x></i> {0}:<br>{1}</br></div>',
+        args = { playerName, msg }
+    })
+end, false)
+
+
+RegisterCommand('unicorn', function(source, args, rawCommand)
     local playerName = GetCharacterName(source)
-    local msg = rawCommand:sub(11)
+    local msg = rawCommand:sub(9)
     TriggerClientEvent('chat:addMessage', -1, {
         template = '<div style="padding: 1.1vw; margin: 1.0vw; background-color: rgba(216, 45, 199, 0.7);border-radius:12px;"><i class="far fa-calendar-check" size: 7x></i> ^0Unicorn:<br>{0}</br></div>',
         args = { msg }
     })
 end, false)
 
+
+--[[
 RegisterCommand('admins',function(source, args, rawCommand)
     local name = GetCharacterName(source)
     local msg = rawCommand:sub(10)
@@ -50,17 +49,6 @@ RegisterCommand('admins',function(source, args, rawCommand)
 end, false]]--
 
 
-AddEventHandler('__cfx_internal:commandFallback', function(command)
-    local name = GetPlayerName(source)
-
-    TriggerEvent('chatMessage', source, name, '/' .. command)
-
-    if not WasEventCanceled() then
-        TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, '/' .. command) 
-    end
-
-    CancelEvent()
-end)
 
 function GetCharacterName(source)
 	local result = MySQL.Sync.fetchAll('SELECT firstname, lastname FROM users WHERE identifier = @identifier', {
